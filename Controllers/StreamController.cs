b161b44f460cd736738dab2b5f425a58ed479bef
Carlos Sanchez <randomouscrap98@aol.com>
2020-01-26 04:29:17 -0500

    Starting to set up stuff

---
+++ Controllers/StreamController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace stream.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StreamController : ControllerBase
+    {
+        private readonly ILogger<StreamController> _logger;
+
+        public StreamController(ILogger<StreamController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return "wow";
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]string data)
+        {
+            return Ok(); //NotFound();
+        }
+    }
+}
